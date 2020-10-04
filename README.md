004c99c03e6d6290712e0dc1311985804559d833
bllackdev <rapha_rmm@hotmail.com>
2020-10-04 18:20:32 -0300

    estrutura básica

---
+++ README.md
@@ -0,0 +1,17 @@
+# Blog Pessoal
+
+Neste projeto você terá o desafio de construir uma API com Json-Server e consuma os dados em um feed de mensagens desenvolvido em Angular.
+
+### Back-End
+
+- npm init - y
+- npm i json-server
+
+
+### Front-End
+
+#### Instalação Angular
+- npm i -g @angular/cli
+- ng new blog
+- cd blog 
+- ng serve
---
+++ frontend/blog/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeedComponent } from './feed/feed.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    FeedComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
